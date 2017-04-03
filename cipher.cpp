8ed493a1cdd0d2f5bb51c303f6b09a38db3e7967
name <6185541513@protonmail.com>
2017-04-03 08:59:47 +0200

    encryption function in progress

--- cipher.cpp
+++ cipher.cpp
@@ -3,11 +3,13 @@
 #include <iomanip>
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 void createLetterMatrix();
 void createRandomNumberString();
 void encrypt(string msg);
+char digitToChar(int n);
 int charToDigit(char c);
 int charToSingleDigit(char c);
 void log(string msg);
@@ -15,6 +17,7 @@ void log(string msg);
 const char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char **letterMatrix = new char*[10];
 string randNumStr;
+vector<int> randNumVec;
 string encryptStr;
 
 int main()
@@ -83,6 +86,16 @@ void createRandomNumberString()
 	  oss << tmp;
 	  randNumStr += oss.str();
 	}
+      for(int i = 0; i < hexStr.length(); i++)
+	{
+	  stringstream ss;
+	  ostringstream oss;
+	  int tmp;
+	  char c = hexStr[i];
+	  ss << c;
+	  ss >> hex >> tmp;
+	  randNumVec.push_back(tmp);
+	}
       
       infile.close();
       
@@ -96,10 +109,12 @@ void createRandomNumberString()
 
 void encrypt(string msg)
 {
-  //message -> digits
+  //message -> literal digits
   string::iterator it;
   //string numMsg;
-  int numMsgArr[msg.length()];
+  int size = msg.length();
+  int numMsgArr[size];
+  vector<int> numMsgVec(size);
   
   for(int i = 0; i < msg.length(); i++)
     {
@@ -108,12 +123,32 @@ void encrypt(string msg)
       int n = charToDigit(c);//charToSingleDigit(c);
       //oss << i;
       //numMsg += oss.str();
-      numMsgArr[i] = n; 
+      numMsgArr[i] = n;
+      numMsgVec[i] = n;
     }
-  //log(msg + "=" + numMsg);
 
-  //digits -> encrypted
-  
+  //literal digits -> encrypted digits
+  vector<int> encryptNumMsgVec(size);
+  for(int i = 0; i < size; i++)
+    {
+      encryptNumMsgVec[i] = numMsgVec[i] + randNumVec[i];
+    }
+
+  //encrypted digits -> encrypted message
+  char c = digitToChar(30);
+  cout << "c: " <<c;
+}
+
+//digit -> char
+char digitToChar(int n)
+{
+  //assure digit in letter range
+  while(n >= (sizeof letters))
+    {
+      n -= (sizeof letters)-1;
+    }
+
+  return letters[n];
 }
 
 //char -> digit
